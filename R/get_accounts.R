b9b637022ffd85af949b64818b6adcd783f0f19f
Marcel Schliebs <marcel.schliebs@oii.ox.ac.uk>
2021-07-05 14:01:48 +0200

    remove NAs from exported "latest" file

--- R/get_accounts.R
+++ R/get_accounts.R
@@ -1,13 +1,17 @@
 get_accounts <- function(at_date = lubridate::now()){
   
-  accs <- readr::read_csv("extdata/prc_diplomats_twitter_historical.csv")
+  accs <- readr::read_csv("inst/extdata/prc_diplomats_twitter_historical.csv")
   
   out_df <- 
     accs %>% 
-    dplyr::filter(is.na(ended_post) | as.Date(ended_post) > at_date)
+    dplyr::filter(is.na(ended_post) | as.Date(ended_post) > at_date) %>% 
+    dplyr::select(1:8)
+  
+  out_df[is.na(out_df)] <- 
+    ""
   
   return(out_df)
   
 }
 
-#write.csv(out_df,"inst/extdata/prc_diplomats_twitter_2021-07-05.csv")
+#write.csv(out_df,"inst/extdata/prc_diplomats_twitter_latest.csv")
