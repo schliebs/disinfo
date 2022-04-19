60d79a38d10521c030fcd525837f0b88c16a77f5
schliebs <m.schliebs@zeppelin-university.net>
2022-04-19 14:11:47 +0200

    cn dips update

--- R/get_accounts.R
+++ R/get_accounts.R
@@ -1,79 +1,59 @@
 library(tidyverse)
 
-get_accounts <- function(at_date = lubridate::now()){
+# todo: suppress warnings being printed
+get_accounts <- function(country,group){
   
-  accs <- readr::read_csv("inst/extdata/prc/prc_diplomats_twitter_historical.csv")
-  accs <- readr::read_csv("inst/extdata/russia/russian_diplomats_twitter_historical.csv")
+  at_date = lubridate::now()
   
-  out_df <- 
-    accs %>% 
-    dplyr::filter(is.na(ended_post) | as.Date(ended_post) > at_date) %>% 
-    dplyr::select(1:9) %>% select(-user_id)
+  if("CN" %in% country & "diplomats" %in% group){
+    
+    at_date <- lubridate::today()
+    accs_cn <- readr::read_csv("inst/extdata/cn/cn_diplomats_twitter_historical.csv")
+    labs_cn <- readr::read_csv("inst/extdata/cn/platform_labels_twitter_cn_dips.csv")
+    dips_cn <- readr::read_csv("inst/extdata/cn/twitter_cn_dips.csv")
+    
+    out_df <- 
+      accs_cn %>% 
+      dplyr::filter(is.na(ended_post) | as.Date(ended_post) > at_date) %>% 
+      dplyr::select(1:9) %>% select(-user_id)
+    out_df$ISO2[out_df$country == "Namibia"] <- "NA"
+    
+    res <- 
+      out_df %>%  filter(!is.na(twitter_handle) & twitter_handle != "") %>% select(1:4) %>%  mutate(match = str_to_lower(twitter_handle)) %>%
+      left_join(labs_cn %>% mutate(match = str_to_lower(handle)) %>% select(match,gov_label),by = "match") %>%
+      left_join(dips_cn %>% mutate(match = str_to_lower(screen_name)), by = "match") %>% select(-match,-date_scraped)
+    
+    print(paste0(nrow(res)," Chinese diplomats and government accounts. Twitter transparency labels as of ",labs_cn$date_scraped[1],". Account data as of ", dips_cn$date_scraped[1],"."))
+    
+    return(res)
+  }
   
-  #out_df[is.na(out_df)] <- ""
-  
-  return(out_df)
+  if("RU" %in% country & "diplomats" %in% group){
+    
+    at_date <- lubridate::today()
+    accs_ru <- readr::read_csv("inst/extdata/ru/ru_diplomats_twitter_historical.csv")
+    labs_ru <- readr::read_csv("inst/extdata/ru/platform_labels_twitter_ru_dips.csv")
+    dips_ru <- readr::read_csv("inst/extdata/ru/twitter_ru_dips.csv")
+    
+    out_df <- 
+      accs_ru %>% 
+      dplyr::filter(is.na(ended_post) | as.Date(ended_post) > at_date) %>% 
+      dplyr::select(1:9) %>% select(-user_id)
+    out_df$ISO2[out_df$country == "Namibia"] <- "NA"
+    
+    res <- 
+      out_df %>%  filter(!is.na(twitter_handle) & twitter_handle != "") %>% select(1:4) %>%  mutate(match = str_to_lower(twitter_handle)) %>%
+      left_join(labs_ru %>% mutate(match = str_to_lower(handle)) %>% select(match,gov_label),by = "match") %>%
+      left_join(dips_ru %>% mutate(match = str_to_lower(screen_name)), by = "match") %>% select(-match,-date_scraped)
+    
+    print(paste0(nrow(res)," Russian diplomats and government accounts. Twitter transparency labels as of ",labs_cn$date_scraped[1],". Account data as of ", dips_cn$date_scraped[1],"."))
+    
+    return(res)
+  }
   
 }
 
-#write.csv(out_df,"inst/extdata/prc_diplomats_twitter_latest.csv")
-write.csv(out_df,"inst/extdata/russia/russia_diplomats_twitter_latest.csv")
-write.csv(out_df,"inst/extdata/prc/prc_diplomats_twitter_latest.csv")
 
-
-######################
-
-library(tidyverse)
-
-df <- 
-  readxl::read_excel("C:/Users/ms/Desktop/hamilton_russia.xlsx") %>% 
-  mutate(handle = `User Account` %>% str_remove_all("@") %>% str_to_lower)
-
-df3 <- 
-  readxl::read_excel("C:/Users/ms/Desktop/hamilton_china.xlsx") %>% 
-  mutate(handle = `User Account` %>% str_remove_all("@") %>% str_to_lower)
-
-df2 <- 
-  read_csv("inst/extdata/russia/russia_diplomats_twitter_latest.csv") %>% 
-  mutate(handle = twitter_handle %>% str_to_lower)
-
-df4 <- 
-  read_csv("inst/extdata/prc/prc_diplomats_twitter_latest.csv") %>% 
-  mutate(handle = twitter_handle %>% str_to_lower)
-
-# hamilton but not my
-df$handle %>% .[!. %in% df2$handle]
-
-df3$handle %>% .[!. %in% df4$handle]
-
-# my list but not hamilton
-df2$handle %>% .[!is.na(.)] %>% .[!. %in% df$handle] 
-
-df4$handle %>% .[!is.na(.)] %>% .[!. %in% df3$handle] 
-
-
-users  <- df4$handle %>% unique() 
-
-## get users data
-usr_df <- rtweet::lookup_users(users)
-
-users %>% .[!. %in% (usr_df$screen_name %>% str_to_lower)]
-
-#############################
-library(rtweet)
-library(tidyverse)
-## CNN twitter accounts
-users <- df2$handle %>% unique() 
-
-users %>% .[1:50] %>% paste0("@",.,collapse= " ")
-
-
-
-## create CNN-accounts list with 9 total users
-(ru_lst <- rtweet::post_list(users = users[201:250],   
-                             list_id = "1500254439600439298",
-                            # private = TRUE,
-                             token = ms_token))
 
 
 
