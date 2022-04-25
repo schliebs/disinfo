a7e246e425a0f97a96276685bf6bfaa8bb82413b
schliebs <m.schliebs@zeppelin-university.net>
2022-04-25 11:49:39 +0100

    update documentation

--- R/get_accounts.R
+++ R/get_accounts.R
@@ -1,16 +1,40 @@
 library(tidyverse)
 
-# todo: suppress warnings being printed
-get_accounts <- function(country,group){
+#' Get lists of Russian or Chinese Diplomats
+#'
+#' @param country currently supports "RU" and "CN" (or both: c("RU","CN"))
+#' @param group currently supports "diplomats"
+#' @return returns data.frame with twitter handles and details on dipomats
+#' @references tbc
+#' @note tbc
+#' @examples
+#' options(tidyverse.quiet = TRUE)
+#' library(tidyverse)
+#' ru_accs <- 
+#'   get_accounts(country = "RU",group = "diplomats")
+#' 
+#' head(ru_accs,10)
+#' 
+#' ru_accs %>%
+#'   group_by(cat) %>%
+#'   mutate(n_cat = n()) %>%
+#'   group_by(cat,n_cat,gov_label = gov_label != "") %>%
+#'   summarise(n = n()) %>%
+#'   mutate(perc = n/n_cat) %>% filter(gov_label == T)
+#'
+#' @export
+get_accounts <- function(country,group = c("diplomats")){
   
   at_date = lubridate::now()
+  res_cn <- NULL
+  res_ru <- NULL
   
   if("CN" %in% country & "diplomats" %in% group){
     
     at_date <- lubridate::today()
-    accs_cn <- readr::read_csv("inst/extdata/cn/cn_diplomats_twitter_historical.csv")
-    labs_cn <- readr::read_csv("inst/extdata/cn/platform_labels_twitter_cn_dips.csv")
-    dips_cn <- readr::read_csv("inst/extdata/cn/twitter_cn_dips.csv")
+    accs_cn <- readr::read_csv(file.path(path.package("disinfo"),"extdata/cn/cn_diplomats_twitter_historical.csv"))
+    labs_cn <- readr::read_csv(file.path(path.package("disinfo"),"extdata/cn/platform_labels_twitter_cn_dips.csv"))
+    dips_cn <- readr::read_csv(file.path(path.package("disinfo"),"extdata/cn/twitter_cn_dips.csv"))
     
     out_df <- 
       accs_cn %>% 
@@ -18,22 +42,23 @@ get_accounts <- function(country,group){
       dplyr::select(1:9) %>% select(-user_id)
     out_df$ISO2[out_df$country == "Namibia"] <- "NA"
     
-    res <- 
+    res_cn <- 
       out_df %>%  filter(!is.na(twitter_handle) & twitter_handle != "") %>% select(1:4) %>%  mutate(match = str_to_lower(twitter_handle)) %>%
       left_join(labs_cn %>% mutate(match = str_to_lower(handle)) %>% select(match,gov_label),by = "match") %>%
-      left_join(dips_cn %>% mutate(match = str_to_lower(screen_name)), by = "match") %>% select(-match,-date_scraped)
+      left_join(dips_cn %>% mutate(match = str_to_lower(screen_name)), by = "match") %>% select(-match,-date_scraped) %>% 
+      mutate(data_scraped = dips_cn$date_scraped[1],
+             labels_scraped = labs_cn$date_scraped[1])
     
-    print(paste0(nrow(res)," Chinese diplomats and government accounts. Twitter transparency labels as of ",labs_cn$date_scraped[1],". Account data as of ", dips_cn$date_scraped[1],"."))
+    print(paste0(nrow(res_cn)," Chinese diplomats and government accounts. Twitter transparency labels as of ",labs_cn$date_scraped[1],". Account data as of ", dips_cn$date_scraped[1],"."))
     
-    return(res)
   }
   
   if("RU" %in% country & "diplomats" %in% group){
     
     at_date <- lubridate::today()
-    accs_ru <- readr::read_csv("inst/extdata/ru/ru_diplomats_twitter_historical.csv")
-    labs_ru <- readr::read_csv("inst/extdata/ru/platform_labels_twitter_ru_dips.csv")
-    dips_ru <- readr::read_csv("inst/extdata/ru/twitter_ru_dips.csv")
+    accs_ru <- readr::read_csv(file.path(path.package("disinfo"),"extdata/ru/ru_diplomats_twitter_historical.csv"))
+    labs_ru <- readr::read_csv(file.path(path.package("disinfo"),"extdata/ru/platform_labels_twitter_ru_dips.csv"))
+    dips_ru <- readr::read_csv(file.path(path.package("disinfo"),"extdata/ru/twitter_ru_dips.csv"))
     
     out_df <- 
       accs_ru %>% 
@@ -41,15 +66,19 @@ get_accounts <- function(country,group){
       dplyr::select(1:9) %>% select(-user_id)
     out_df$ISO2[out_df$country == "Namibia"] <- "NA"
     
-    res <- 
+    res_ru <- 
       out_df %>%  filter(!is.na(twitter_handle) & twitter_handle != "") %>% select(1:4) %>%  mutate(match = str_to_lower(twitter_handle)) %>%
       left_join(labs_ru %>% mutate(match = str_to_lower(handle)) %>% select(match,gov_label),by = "match") %>%
-      left_join(dips_ru %>% mutate(match = str_to_lower(screen_name)), by = "match") %>% select(-match,-date_scraped)
+      left_join(dips_ru %>% mutate(match = str_to_lower(screen_name)), by = "match") %>% select(-match,-date_scraped) %>% 
+      mutate(data_scraped = dips_ru$date_scraped[1],
+             labels_scraped = labs_ru$date_scraped[1])
     
-    print(paste0(nrow(res)," Russian diplomats and government accounts. Twitter transparency labels as of ",labs_cn$date_scraped[1],". Account data as of ", dips_cn$date_scraped[1],"."))
+    print(paste0(nrow(res_ru)," Russian diplomats and government accounts. Twitter transparency labels as of ",labs_ru$date_scraped[1],". Account data as of ", dips_ru$date_scraped[1],"."))
     
-    return(res)
   }
+  
+  res <- bind_rows(res_cn,res_ru)
+  return(res)
   
 }
 
--- README.md
+++ README.md
@@ -1,3 +1,28 @@
-# disinfo
-Tools, Datasets, and Seedlists for Disinformation Studies
 
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# disinfo
+
+<!-- # pkgdown <img src="man/figures/logo.png" align="right" alt="" width="120" />-->
+<!-- badges: start 
+[![CRAN Status](https://www.r-pkg.org/badges/version/pkgdown)](https://cran.r-project.org/package=pkgdown){.pkgdown-release}
+[![R-CMD-check](https://github.com/r-lib/pkgdown/workflows/R-CMD-check/badge.svg)](https://github.com/r-lib/pkgdown/actions){.pkgdown-devel}
+[![Codecov test coverage](https://codecov.io/gh/r-lib/pkgdown/branch/main/graph/badge.svg)](https://app.codecov.io/gh/r-lib/pkgdown?branch=main)
+badges: end -->
+
+disinfo opening paragraph. Learn more in `vignette("disinfo")`.
+
+## Installation
+
+<div class=".disinfo-devel">
+
+``` r
+# Install development version from GitHub
+devtools::install_github("schliebs/disinfo")
+```
+
+</div>
+
+## Usage
+
+…
